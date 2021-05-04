66b33ac53f185bafe630d7a2cac9f8a6c9027e69
Ash Shah <ashni@uw.edu>
2021-05-04 15:06:03 -0700

    Initial Commit

---
+++ iKid/iKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidApp.swift
+//  iKid
+//
+//  Created by Ashni Shah on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct iKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
